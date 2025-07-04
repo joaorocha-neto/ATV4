3f5de1b7401d52fa7c0dd062b8011459a79ff160
JOAO DA ROCHA BEZERRA NETO <a95574143@rn.senac.local>
2025-07-04 11:52:35 -0300

    Busca

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
 import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Minhas Tarefas</Text>
-      <TextInput style={styles.input} placeholder='Digite uma nova tarefa...' />
+      <Busca/>
     </View>
+
     
   );
+}
+
+function Busca() {
+  const [inputText, setInputText] = useState('');
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setInputText}
+        value={inputText}
+        placeholder='Digite uma nova tarefa...'
+      />
+
+      <Text style={styles.texto}>{inputText}</Text>
+    </View>
+
+  )
 }
 
 const styles = StyleSheet.create({
@@ -30,5 +50,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#fff"
 
+  },
+
+  texto: {
+    color: "#fff"
   }
 });
