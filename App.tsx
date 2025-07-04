20751349747a6bd7b6d8e51d79a4f29f9f763bda
JOAO DA ROCHA BEZERRA NETO <a95574143@rn.senac.local>
2025-07-04 11:16:44 -0300

    Primeiro commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Minhas Tarefas</Text>
+      <TextInput style={styles.input} placeholder='Digite uma nova tarefa...' />
     </View>
+    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  title: {
+    color: "#fff",
+    fontSize: 30
+  },
+
+  input: {
+    padding: 5,
+    color: "#fff",
+    borderWidth: 1,
+    borderColor: "#fff"
+
+  }
 });
